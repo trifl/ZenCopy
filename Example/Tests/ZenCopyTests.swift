7d60baeb3c0e12df8d15e3cbe102136d070f7444
JP McGlone <jp@trifl.co>
2016-02-18 19:31:35 -0800

    cleanup and more tests around args

--- Example/Tests/ZenCopyTests.swift
+++ Example/Tests/ZenCopyTests.swift
@@ -4,15 +4,21 @@ import Quick
 import Nimble
 import ZenCopy
 
-class TableOfContentsSpec: QuickSpec {
+class ZenCopyTests: QuickSpec {
     override func spec() {
         let copyManager = ZenCopy.manager
         
         copyManager.copy["en"] = [String:[CopyComponent]]()
         copyManager.copy["en"]!["test1"] = ["Hello world!"]
-        copyManager.copy["en"]!["test2"] = ["Hello ", "world!".style("")]
+        copyManager.copy["en"]!["test2"] = ["Hello ", "world!".style("hulk")]
         copyManager.copy["en"]!["test3"] = ["Hello $0!"]
         copyManager.copy["en"]!["test4"] = ["Hello ", "$0", "!"]
+        copyManager.copy["en"]!["test5"] = ["@$0: hey @$1, it's me @$0"]
+
+        
+        //styles
+        copyManager.styles["hulk"] = Style(color: .greenColor(), font: .systemFontOfSize(200))
+        copyManager.styles["tiny"] = Style(font: .systemFontOfSize(8))
         
         describe("ZenCopy") {
             it("should work with key'") {
@@ -26,6 +32,9 @@ class TableOfContentsSpec: QuickSpec {
             }
             it("should work on styled value works with multiple components args") {
                 expect(copyManager.string(key: "test4", args: ["John"])) == "Hello John!"
+            }
+            it("should work with multiple args, even duplicates") {
+                expect(copyManager.string(key: "test5", args: ["jpmcglone", "you"])) == "@jpmcglone: hey @you, it's me @jpmcglone"
             }
             it("string on the fly should work") {
                 expect(copyManager.string(["Hello $0!"], args: ["JPM"])) == "Hello JPM!"
--- Pod/Classes/Style.swift
+++ Pod/Classes/Style.swift
@@ -1,6 +1,11 @@
 import Foundation
 
 public struct Style {
-    var color: UIColor?
-    var font: UIFont?
+    public var color: UIColor?
+    public var font: UIFont?
+    
+    public init(color: UIColor? = nil, font: UIFont? = nil) {
+        self.color = color
+        self.font = font
+    }
 }
