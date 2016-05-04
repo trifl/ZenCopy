5dd60103254ab6b30abc6cdee4b4000c6d95720d
JP McGlone <jp@trifl.co>
2016-05-04 12:44:54 -0700

    added tokenized option to setting style. this needs some refactor, but this is a good update

--- Pod/Classes/NSMutableAttributedString+Additions.swift
+++ Pod/Classes/NSMutableAttributedString+Additions.swift
@@ -10,34 +10,38 @@ public extension NSMutableAttributedString {
         return addStyle(style, regex: regex)
     }
     
-    public func addStyle(style: Style, regex: String) -> [NSTextCheckingResult]? {
-        return self.style(style, regex: regex, replace: false)
+    public func addStyle(style: Style, regex: String, tokenized: Bool = true) -> [NSTextCheckingResult]? {
+        return self.style(style, regex: regex, tokenized: tokenized, replace: false)
     }
     
     // Set (replace) style
-    public func setStyle(named styleName: String, regex: String) -> [NSTextCheckingResult]? {
+    public func setStyle(named styleName: String, regex: String, tokenized: Bool = true) -> [NSTextCheckingResult]? {
         let style = Style()
         for styleName in ZenCopy.manager.styleNamesFromStyleString(styleName) {
             if let s = ZenCopy.manager.config.styles?(name: styleName) {
                 style.append(s)
             }
         }
-        return self.setStyle(style, regex: regex)
+        return self.setStyle(style, regex: regex, tokenized: tokenized)
     }
     
-    public func setStyle(style: Style, regex: String) -> [NSTextCheckingResult]? {
-        return self.style(style, regex: regex, replace: true)
+    public func setStyle(style: Style, regex: String, tokenized: Bool = true) -> [NSTextCheckingResult]? {
+        return self.style(style, regex: regex, tokenized: tokenized, replace: true)
+    }
+    
+    public func style(style: Style, regex: String, tokenized: Bool = true) -> [NSTextCheckingResult]? {
+        return self.style(style, regex: regex, tokenized: tokenized, replace: true)
     }
     
     // MARK: - Private
-    private func style(style: Style, regex: String, replace: Bool) -> [NSTextCheckingResult]? {
+    private func style(style: Style, regex: String, tokenized: Bool, replace: Bool) -> [NSTextCheckingResult]? {
         do {
             let regularExpression = try NSRegularExpression(pattern: regex, options: NSRegularExpressionOptions(rawValue: 0))
             let range = NSRange(location: 0, length: self.length)
             
             let matches = regularExpression.matchesInString(self.string, options: NSMatchingOptions(rawValue: 0), range: range)
             for match in matches {
-                let range = match.range
+                let range = (tokenized ? match.rangeAtIndex(match.numberOfRanges-1) : match.range)
                 
                 let attributes = ZenCopy.manager.attributesForStyle(style)
                 if replace {
@@ -46,6 +50,7 @@ public extension NSMutableAttributedString {
                     self.addAttributes(attributes, range: range)
                 }
             }
+            
             return matches
         } catch _ {
             
