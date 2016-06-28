be5e3ae133ed0b5bd2e09c96f796764d771321d1
JP McGlone <jp@trifl.co>
2016-06-28 10:58:22 -0700

    can now style with nsdatadetectortypes

--- Pod/Classes/NSMutableAttributedString+Additions.swift
+++ Pod/Classes/NSMutableAttributedString+Additions.swift
@@ -1,3 +1,5 @@
+import Foundation
+
 public extension NSMutableAttributedString {
     // Add style
     public func addStyle(named styleName: String, regex: String) -> [NSTextCheckingResult]? {
@@ -14,7 +16,6 @@ public extension NSMutableAttributedString {
         return self.style(style, regex: regex, tokenized: tokenized, replace: false)
     }
     
-    // Set (replace) style
     public func setStyle(named styleName: String, regex: String, tokenized: Bool = true) -> [NSTextCheckingResult]? {
         let style = Style()
         for styleName in ZenCopy.manager.styleNamesFromStyleString(styleName) {
@@ -33,6 +34,33 @@ public extension NSMutableAttributedString {
         return self.style(style, regex: regex, tokenized: tokenized, replace: true)
     }
     
+    // Set (replace) style
+    public func setStyle(named styleName: String, dataDetector: NSDataDetector, tokenized: Bool = true, replace: Bool = true) -> [NSTextCheckingResult]? {
+        let matches = dataDetector.matchesInString(string, options: [], range: NSRange(location: 0, length: string.characters.count))
+        let style = Style()
+        for styleName in ZenCopy.manager.styleNamesFromStyleString(styleName) {
+            if let s = ZenCopy.manager.config.styles?(name: styleName) {
+                style.append(s)
+            }
+        }
+        return self.style(style, matches: matches, tokenized: tokenized, replace: replace)
+    }
+    
+    private func style(style: Style, matches: [NSTextCheckingResult], tokenized: Bool, replace: Bool) -> [NSTextCheckingResult]? {
+        for match in matches {
+            let range = (tokenized ? match.rangeAtIndex(match.numberOfRanges-1) : match.range)
+            
+            let attributes = ZenCopy.manager.attributesForStyle(style)
+            if replace {
+                self.setAttributes(attributes, range: range)
+            } else if let attributes = attributes {
+                self.addAttributes(attributes, range: range)
+            }
+        }
+        
+        return matches
+    }
+    
     // MARK: - Private
     private func style(style: Style, regex: String, tokenized: Bool, replace: Bool) -> [NSTextCheckingResult]? {
         do {
@@ -40,18 +68,8 @@ public extension NSMutableAttributedString {
             let range = NSRange(location: 0, length: self.length)
             
             let matches = regularExpression.matchesInString(self.string, options: NSMatchingOptions(rawValue: 0), range: range)
-            for match in matches {
-                let range = (tokenized ? match.rangeAtIndex(match.numberOfRanges-1) : match.range)
-                
-                let attributes = ZenCopy.manager.attributesForStyle(style)
-                if replace {
-                    self.setAttributes(attributes, range: range)
-                } else if let attributes = attributes {
-                    self.addAttributes(attributes, range: range)
-                }
-            }
             
-            return matches
+            return self.style(style, matches: matches, tokenized: tokenized, replace: replace)
         } catch _ {
             
         }
